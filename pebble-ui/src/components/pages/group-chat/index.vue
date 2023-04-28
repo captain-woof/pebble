fd7132e2a43f5c207b265276b317fb570fd1e713
captain-woof <captainwoof.official@gmail.com>
2023-04-28 22:58:27 +0530

    Group deselected upon leaving page

--- pebble-ui/src/components/pages/group-chat/index.vue
+++ pebble-ui/src/components/pages/group-chat/index.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import { onUnmounted } from "vue";
 import FullPageSection from "@components/atoms/full-page-section.vue";
-import GroupChat from "@components/molecules/group-chat/index.vue"
+import GroupChat from "@components/molecules/group-chat/index.vue";
+import usePebbleStore from "@store/pebble";
+
+// States
+const pebbleStore = usePebbleStore();
+
+// Methods
+onUnmounted(() => {
+    pebbleStore.deselectGroupSelected();
+})
 </script>
 
 <template>
